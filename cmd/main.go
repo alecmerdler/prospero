b5d71f98144ad2ba21c50d31e88fe8048377870f
alecmerdler <alecmerdler@gmail.com>
2019-05-05 01:19:25 -0400

    starting rewrite

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("running prospero")
+
+	// TODO(alecmerdler): Set up HTTP server for static assets
+
+	// TODO(alecmerdler): Set up HTTP server for API endpoints
+
+	// TODO(alecmerdler): Set up WebSocket endpoints
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alecmerdler/prospero
+
+go 1.12
